3af1ff5acab8759c9a2802c1d08d83e51de36092
erwin@dehaantjes.nl <erwin@dehaantjes.nl@be53e3b4-4504-43aa-b4cb-0fa9024e2f7e>
2010-02-13 11:03:10 +0000

    Upload 1

---
+++ SAND/trunk/reSAND/reSAND.cpp
@@ -0,0 +1,18 @@
+// reSAND.cpp : main project file.
+
+#include "stdafx.h"
+#include "Form1.h"
+
+using namespace reSAND;
+
+[STAThreadAttribute]
+int main(array<System::String ^> ^args)
+{
+	// Enabling Windows XP visual effects before any controls are created
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false); 
+
+	// Create the main window and run it
+	Application::Run(gcnew Form1());
+	return 0;
+}
