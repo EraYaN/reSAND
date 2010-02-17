54ce47d9cd1995b6673e1eaa9e3535c3b745ae9c
erwin@dehaantjes.nl <erwin@dehaantjes.nl@be53e3b4-4504-43aa-b4cb-0fa9024e2f7e>
2010-02-17 12:22:55 +0000

    DirextX 10 Extracted and put in extra class

--- reSAND/trunk/reSAND/reSAND.cpp
+++ reSAND/trunk/reSAND/reSAND.cpp
@@ -1,205 +1,115 @@
-#include <windows.h> 
-#include <d3d10.h> 
-#include <d3dx10.h>
+#include <windows.h>
+#include <tchar.h>
+#include <stdio.h>
 
-struct Vertex
+#include "dxManager.h"
+
+/*******************************************************************
+* Global Variables
+*******************************************************************/
+HWND hWnd;					//window handle
+int windowWidth = 800;	
+int windowHeight = 600;
+
+//directX manager
+dxManager dx;
+
+/*******************************************************************
+* Main Window Procedure - handles application events
+*******************************************************************/
+LRESULT CALLBACK wndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
-	float x, y, z;
-};
-
-struct ColorVertex {
-	float x, y, z;
-	DWORD color;
-};
-struct NormalTexVertex {
-	float x, y, z;
-	float nx, ny, nz;
-	float tu, tv;   // Texture coordinates
-};
-ColorVertex vertexes[];	
-
-
-#define FVF_COLOR(D3DFVF_XYZ | D3DFVF_DIFFUSE)
-
-
-
-#define Error(X) MessageBox(NULL, X, "Error", MB_OK)
-
-class Application
-{
-public:
-
-    Application();
-    ~Application();
-
-    bool CreateD3DWindow(int width, int height);
-
-    bool Initialize(); 
-    void Render(float deltaTime);
-    void MainLoop();
-
-private:
-
-    static LRESULT CALLBACK WndProc( HWND hWnd , UINT message , 
-                                     WPARAM wParam , LPARAM lParam);
-
-    HWND                        hWnd; 
-    IDXGISwapChain*                pSwapChain;
-    ID3D10RenderTargetView*        pRenderTargetView;
-    ID3D10Device*                pDevice; 
-    int                            width, height;
-};
-
-#pragma comment (lib, "d3d10.lib")
-#pragma comment (lib, "d3dx10.lib")
-
-Application::Application()
-{
-}
-
-Application::~Application()
-{
-}
-
-LRESULT CALLBACK Application::WndProc( HWND hWnd , UINT message , 
-                                       WPARAM wParam , LPARAM lParam)
-{ 
-    switch(message){ 
-    case WM_CLOSE: 
-    case WM_DESTROY: 
-        PostQuitMessage(0); 
-        break;; 
-    } 
-    return DefWindowProc(hWnd,message,wParam,lParam); 
-} 
-
-bool Application::CreateD3DWindow(int width, int height)
-{ 
-    this->width = width;
-    this->height = height;
-    
-    // Create A Window Class Structure 
-    WNDCLASSEX wc; 
-    ZeroMemory(&wc, sizeof(wc));        
-    wc.cbSize = sizeof(wc);                
-    wc.hInstance = GetModuleHandle(NULL);        
-    wc.lpfnWndProc = WndProc;                    
-    wc.lpszClassName = "reSAND";                        
-    wc.style = CS_HREDRAW | CS_VREDRAW;            
-    
-    // Register Window Class 
-    RegisterClassEx(&wc); 
-    
-    // Create Window 
-    hWnd = CreateWindowEx(0, "reSAND", 
-           "reSAND", 
-           WS_OVERLAPPEDWINDOW|WS_VISIBLE, CW_USEDEFAULT, 
-           CW_USEDEFAULT, width, height, NULL,NULL,wc.hInstance,0);
-    
-    return true; 
-} 
-
-bool Application::Initialize()
-{
-    DXGI_SWAP_CHAIN_DESC swapChainDesc;
-    ZeroMemory( &swapChainDesc, sizeof(swapChainDesc) );
-    swapChainDesc.BufferCount = 1;
-    swapChainDesc.BufferDesc.Width = width;
-    swapChainDesc.BufferDesc.Height = height;
-    swapChainDesc.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
-    swapChainDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
-    swapChainDesc.OutputWindow = hWnd;
-    swapChainDesc.SampleDesc.Count = 1;
-    swapChainDesc.SampleDesc.Quality = 0;
-    swapChainDesc.Windowed = TRUE;
-
-    if( FAILED( D3D10CreateDeviceAndSwapChain( NULL, 
-        D3D10_DRIVER_TYPE_HARDWARE, NULL, 
-        0, D3D10_SDK_VERSION, &swapChainDesc, 
-        &pSwapChain, &pDevice ) ) )
-    {
-        if( FAILED( D3D10CreateDeviceAndSwapChain( NULL, 
-            D3D10_DRIVER_TYPE_REFERENCE, NULL, 
-            0, D3D10_SDK_VERSION, &swapChainDesc, 
-            &pSwapChain, &pDevice ) ) )
-        {
-            Error("Failed to create device and swap chain.");
-            return false;
-        }
-    }
-	pDevice->CreateVertexBuffer(sizeof(TIPOVERTICE), 
-								D3DUSAGE_WRITEONLY,
-								vf,
-								D3DPOOL_MANAGED,
-								&pvb,
-								NULL);
-    ID3D10Texture2D *pBackBuffer;
-    if( FAILED( pSwapChain->GetBuffer( 0, __uuidof( ID3D10Texture2D ), 
-              (LPVOID*)&pBackBuffer ) ) )
-    {
-        Error("Failed to create back buffer.");
-        return false;
-    }
-
-    if(FAILED( pDevice->CreateRenderTargetView( pBackBuffer, 
-               NULL, &pRenderTargetView )))
-    {
-        Error("Failed to create render target view.");
-        return false;
-    }
-
-    pBackBuffer->Release();
-
-    pDevice->OMSetRenderTargets( 1, &pRenderTargetView, NULL );
-
-    D3D10_VIEWPORT vp = {0, 0, width, height, 0, 1};
-    pDevice->RSSetViewports( 1, &vp );
-    
-    return true; 
-} 
-
-void Application::Render(float deltaTime)
-{
-    pDevice->ClearRenderTargetView( pRenderTargetView, 
-                                       D3DXVECTOR4(0, 0, 0, 1) );
+	switch (message) 
+	{
+		// Allow the user to press the escape key to end the application
+        case WM_KEYDOWN	:	switch(wParam)
+							{
+								// Check if the user hit the escape key
+								case VK_ESCAPE : PostQuitMessage(0);
+								break;
+							}
+		
+        break;		
 	
+		// The user hit the close button, close the application
+		case WM_DESTROY	:	PostQuitMessage(0);
+		break;
+	}
+	
+	return DefWindowProc(hWnd, message, wParam, lParam);
 }
-    pSwapChain->Present( 0, 0 );
-} 
 
-void Application::MainLoop()
-{ 
-    MSG msg; 
-    long prevTime = GetTickCount(), curTime = GetTickCount();
-    while(true)
+/*******************************************************************
+* Initialize Main Window
+********************************************************************/
+bool initWindow(HWND &hWnd, HINSTANCE hInstance, int width, int height)
+{
+	WNDCLASSEX wcex;
+
+	wcex.cbSize			= sizeof(WNDCLASSEX);
+	wcex.style			= CS_HREDRAW | CS_VREDRAW;
+	wcex.lpfnWndProc	= (WNDPROC)wndProc;
+	wcex.cbClsExtra		= 0;
+	wcex.cbWndExtra		= 0;
+	wcex.hInstance		= hInstance;
+	wcex.hIcon			= 0;
+	wcex.hCursor		= LoadCursor(NULL, IDC_ARROW);
+	wcex.hbrBackground	= (HBRUSH)(COLOR_WINDOW+1);
+	wcex.lpszMenuName	= NULL;
+	wcex.lpszClassName	= TEXT("DXTutorial1");
+	wcex.hIconSm		= 0;
+	RegisterClassEx(&wcex);
+
+	//Resize the window
+	RECT rect = { 0, 0, width, height };
+    AdjustWindowRect(&rect, WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_MINIMIZEBOX, FALSE);
+
+	//create the window from the class above
+	//disable resizing and correct for extra width and height
+	hWnd = CreateWindow( "reSAND", 
+						 "reSAND - by Erwin de Haan", 
+						 WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_MINIMIZEBOX,
+						 CW_USEDEFAULT, 
+						 CW_USEDEFAULT, 
+						 rect.right - rect.left, 
+						 rect.bottom - rect.top, 
+						 NULL, 
+						 NULL, 
+						 hInstance, 
+						 NULL);
+   
+	//window handle not created
+	if (!hWnd) return false;
+	
+	//if window creation was successful
+	ShowWindow(hWnd, SW_SHOW);
+	UpdateWindow(hWnd);
+	return true;
+}
+
+/*******************************************************************
+* WinMain
+*******************************************************************/
+int APIENTRY _tWinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance, LPTSTR lpCmdLine, int nCmdShow )
+{
+	// Set up the application window
+	if ( !initWindow(hWnd, hInstance, windowWidth, windowHeight)) return 0;
+	
+	//set up directx manager
+	if ( !dx.initialize(&hWnd) ) return 0;
+	
+	// Main message loop
+    MSG msg = {0};
+    while (WM_QUIT != msg.message)
     {
-        if(PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+        while (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE) == TRUE)
         {
-            if(msg.message==WM_QUIT)
-                break;
-
             TranslateMessage(&msg);
-            DispatchMessage(&msg);
-        }
-        else{
-            Render((curTime-prevTime)/1000.f);
-            prevTime = curTime;
-            curTime = GetTickCount();
-        }
+            DispatchMessage(&msg);			
+		}	
+	
+		dx.renderScene();
     }
-} 
 
-INT WINAPI WinMain( HINSTANCE , HINSTANCE , LPSTR , INT )
-{ 
-    Application app;
-    
-    if(app.CreateD3DWindow(640, 480))
-    {
-        if(app.Initialize())
-        {
-            app.MainLoop();
-        }
-    }
-    
-    return 0; 
+	return (int) msg.wParam;
 }
